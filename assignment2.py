e7bd7127a185361fc8f09db05e4d2d1f1f88f96f
prateeksahu10 <pranavsahu810@gmail.com>
2018-08-19 00:10:40 +0530

    this is assignment2

---
+++ assignment2.py
@@ -0,0 +1,29 @@
+# q1 print something
+print("jai bhadrakali")
+
+# q2 concatinate strings
+a=("jai ")
+b=("bhadrakali")
+print(a+b)
+
+# q3 print 3 user variables
+x=input("enter 1st var")
+y=input("enter 2nd var")
+z=input("enter 3rd var")
+print(x,y,z)
+
+# q4 print lets get started
+a1="Let's Get Started"
+print(a1)
+
+# q5 print given value using placeholders
+a2='Chitkara University'
+a3='BTECH'
+a4=75000
+print("University: %s,Course: %s,fees: %d" %(a2,a3,a4))
+
+# q6 find area of circle with user defined radius
+r=float(input("enter radius of the circle"))
+pie=3.14
+area=pie*(r**2)
+print("area of circle is : ",area)
